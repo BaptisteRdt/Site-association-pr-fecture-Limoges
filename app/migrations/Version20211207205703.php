ecb93c1b6cd0dd4e4dd6df50859953f427ac4f6c
xernois <antoine2496@gmail.com>
2021-12-16 13:33:34 +0100

    fix migration

--- app/migrations/Version20211207205703.php
+++ app/migrations/Version20211207205703.php
@@ -21,11 +21,13 @@ final class Version20211207205703 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD mail VARCHAR(255) NOT NULL, ADD date_renewal DATE DEFAULT NULL, ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD cart LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `user` DROP mail, DROP date_renewal, DROP first_name, DROP last_name');
+        $this->addSql('ALTER TABLE `user` DROP cart');
     }
 }
